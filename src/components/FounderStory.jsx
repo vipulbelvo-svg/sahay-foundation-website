436747d38801980eb93296b11af64cd7ddda3593
vipulbelvo-svg <vipul.belvo@gmail.com>
2026-09-25 15:03:47 +0000

    Update FounderStory.jsx

--- src/components/FounderStory.jsx
+++ src/components/FounderStory.jsx
@@ -2,20 +2,17 @@ export default function FounderStory() {
   return (
     <section id="founder-story" className="founder-story">
       <div className="container">
-        <h1>The Story of Founder Hrishikesh</h1>
+        <h1></h1>
 
         <p>
-          A boy was born into a wealthy family in Goregaon, Mumbai.
+          
         </p>
 
         <p>
-          His parents had fallen in love during their school days. His father
-          belonged to Vrindavan, while his mother was from Varanasi...
+          
         </p>
 
         <p>
-          Determined to stay together, they left everything behind and got
-          married without taking any financial support from their families.
         </p>
 
         {/* Isi tarah poori story paragraph-wise paste kar do */}
